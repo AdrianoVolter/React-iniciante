6811669e7dcbc9cd6c4e907a4e1364b57ac5d5c6
Adriano Volter <91354299+AdrianoVolter@users.noreply.github.com>
2023-04-29 11:29:30 -0300

    feat: componente post

--- src/components/Posts.jsx
+++ src/components/Posts.jsx
@@ -1,22 +1,23 @@
+import { Post } from "./Post"
 
 export function Posts () {
+
+    const title = "Postagem 1"
+    const descriptions = "lorem ipsum dolor sit amet, consectetur adipiscing elit post 1."
+    const title2 = "Postagem 2"
+    const descriptions2 = "lorem ipsum dolor sit amet, consectetur adipiscing elit post 2."
+
+
+
   return (
     <div>
         <ul>
-            <li>
-                <h2>Post 1 </h2>
-                <p>
-                    lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                </p>
-            </li>
-            <li>
-                <h2>Post 2 </h2>
-                <p>
-                    lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                </p>
-            </li>
+             <Post titulo={title} paragrafo={descriptions} />
+            <Post titulo={title2} paragrafo={descriptions2} />
+        
         </ul>
-      
+       
+       
     </div>
   )
 }
